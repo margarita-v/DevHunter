80a8fae7295ece1f08a1af369f79bad25cf2e4ae
Margarita <margo.himera@yandex.ru>
2018-02-16 18:13:59 +0300

    UserSchema was added

---
+++ server/app/modules/users/models/index.js
@@ -0,0 +1,6 @@
+import User from './user';
+
+// For easy import of this model from every module
+export {
+    User
+};
---
+++ server/app/modules/users/models/user.js
@@ -0,0 +1,63 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import uniqueValidator from 'mongoose-unique-validator';
+
+mongoose.plugin(uniqueValidator);
+
+const UserSchema = new Schema({
+   email: {
+       type: String,
+       unique: 'User with email "VALUE" already exists',
+       lowercase: true,
+       required: 'Email is required'
+   },
+   password: {
+       type: String,
+       required: 'Password is required'
+   },
+   firstName: {
+       type: String,
+       lowercase: true,
+       required: 'First name is required'
+   },
+   lastName: {
+       type: String,
+       lowercase: true,
+       required: 'Last name is required'
+   }
+}, {
+    // Mongoose will automatically add fields createdAt and updatedAt
+    timestamp: true
+});
+
+/**
+ * Static parameters for a model which will return an array of fields for a user creation
+ */
+UserSchema.statics.createFields = ['email', 'password', 'firstName', 'lastName'];
+
+/**
+ * Handler for a user creation
+ */
+UserSchema.pre('save', function (next) {
+   if (!this.isModified('password')) {
+       return next();
+   }
+
+   const salt = bcrypt.genSaltSync(10);
+   this.password = bcrypt.hashSync(this.password, salt);
+   next();
+});
+
+/**
+ * Function which returns a user with public fields only.
+ */
+UserSchema.statics.findOneWithPublicFields = (params, cb) =>
+    this.findOne(params, cb)
+        .select({
+            // 0 because we don't need to get values of that fields
+            password: 0,
+            _id: 0,
+            __v: 0
+        });
+
+export default mongoose.model('user', UserSchema);
