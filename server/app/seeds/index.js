686e7e8c7b6ba54b9051cc0d54b7789a80fc0ea2
Margarita <margo.himera@yandex.ru>
2018-02-18 20:14:31 +0300

    User seeds were added

--- server/app/seeds/index.js
+++ server/app/seeds/index.js
@@ -1,6 +1,7 @@
-import { MONGO_URI } from "../config";
+import { MONGO_URI } from '../config';
 import mongoose from 'mongoose';
-import mongooseConnector from "../connectors/mongoose-connector";
+import mongooseConnector from '../connectors/mongoose-connector';
+import createFakeUsers from './user-seeds';
 
 initSeeds();
 
@@ -8,6 +9,15 @@ initSeeds();
  * Function for performing connection to the MongoDb and creation of seeds
  */
 async function initSeeds() {
+    // Init connection to the database
     await mongooseConnector(MONGO_URI);
-    mongoose.connection.close();
+
+    // Drop all data
+    const mongoConnection = mongoose.connection;
+    await mongoConnection.dropDatabase();
+
+    // Create fake users and close database connection
+    const users = await createFakeUsers();
+    console.log(users);
+    mongoConnection.close();
 }
---
+++ server/app/seeds/user-seeds.js
@@ -0,0 +1,19 @@
+import faker from 'faker';
+import _ from 'lodash';
+import { User } from '../modules/users';
+
+const COUNT_OF_USERS = 20;
+
+export default function createFakeUsers() {
+    const promises = [];
+    _.times(COUNT_OF_USERS, () => {
+        const userPromise = User.create({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        });
+        promises.push(userPromise);
+    });
+    return Promise.all(promises);
+}
