1bb547718cd31ffefed822d8d154e02aa2fc4631
Margarita <margo.himera@yandex.ru>
2018-02-24 19:36:46 +0300

    Tests for the function of parsing a query for search were added

---
+++ server/app/modules/curriculum-vitae/__tests__/parse-search-query.spec.js
@@ -0,0 +1,41 @@
+import parseSearchQuery from "../helpers/parse-search-query";
+import {MAX_COUNT_OF_RESPONSE_ITEMS, PAGE_NUMBER} from "../constants/pagination";
+
+const EMPTY_RESULT = {
+    title: '',
+    tags: [],
+    size: MAX_COUNT_OF_RESPONSE_ITEMS,
+    page: PAGE_NUMBER,
+};
+
+describe('Tests for the function of parsing a query for search', () => {
+
+    it('Correct parsing of query for search for invalid params', () => {
+        const result = parseSearchQuery({ fakeParam: 'value' });
+        expect(result).toEqual(EMPTY_RESULT);
+    });
+
+    it('Correct parsing of valid query for search', () => {
+        const size = MAX_COUNT_OF_RESPONSE_ITEMS - 1;
+        const page = 50;
+        const query = {
+            title: 'Some title',
+            tags: 'tag1,tag2',
+            size,
+            page,
+        };
+        const result = parseSearchQuery(query);
+        expect(result).toEqual({
+            ...query,
+            tags: query.tags.split(','),
+        });
+    });
+
+    it('Return default page number and size', () => {
+        const result = parseSearchQuery({
+            size: MAX_COUNT_OF_RESPONSE_ITEMS * 2,
+            page: 'invalid',
+        });
+        expect(result).toEqual(EMPTY_RESULT);
+    })
+});
